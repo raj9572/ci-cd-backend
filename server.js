bff4334983ad4ff332ab57667851f6e41e062fbb
rajali <rajali1432@gmail.com>
2025-08-18 00:56:16 +0530

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/api/message", (req, res) => {
   res.json({
